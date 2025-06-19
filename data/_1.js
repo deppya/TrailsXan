1d17f55414a4d7fe755452abaa7311109478296a
deppya <ntepo98@gmail.com>
2025-06-19 20:15:55 +0300

    Add files via upload

---
+++ data/_1.js
@@ -0,0 +1,17 @@
+var json__1 = {
+"type": "FeatureCollection",
+"name": "_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ΟΙΚΙΣ": "Γαλάνη", "ΤΟΠΙΚ": "Τοπική Κοινότητα Γαλάνης" }, "geometry": { "type": "Point", "coordinates": [ 24.758247326826897, 41.093076281503116 ] } },
+{ "type": "Feature", "properties": { "ΟΙΚΙΣ": "Ξάνθη", "ΤΟΠΙΚ": "Δημοτική Κοινότητα Ξάνθης" }, "geometry": { "type": "Point", "coordinates": [ 24.886766343325576, 41.134554160390202 ] } },
+{ "type": "Feature", "properties": { "ΟΙΚΙΣ": "Σταυρούπολη", "ΤΟΠΙΚ": "Τοπική Κοινότητα Σταυρουπό" }, "geometry": { "type": "Point", "coordinates": [ 24.706089707245507, 41.198625610244214 ] } },
+{ "type": "Feature", "properties": { "ΟΙΚΙΣ": "Κάτω Καρυόφυτο", "ΤΟΠΙΚ": "Τοπική Κοινότητα Καρυοφύτο" }, "geometry": { "type": "Point", "coordinates": [ 24.674408020730233, 41.258897038590376 ] } },
+{ "type": "Feature", "properties": { "ΟΙΚΙΣ": "Λειβαδίτης", "ΤΟΠΙΚ": "Τοπική Κοινότητα Καρυοφύτο" }, "geometry": { "type": "Point", "coordinates": [ 24.669895735343093, 41.306317649819682 ] } },
+{ "type": "Feature", "properties": { "ΟΙΚΙΣ": "Δασικό Χωριό", "ΤΟΠΙΚ": "Τοπική Κοινότητα Σταυρουπό" }, "geometry": { "type": "Point", "coordinates": [ 24.713180399135851, 41.331558415338904 ] } },
+{ "type": "Feature", "properties": { "ΟΙΚΙΣ": "Κρωμνικό", "ΤΟΠΙΚ": "Τοπική Κοινότητα Κρωμνικό" }, "geometry": { "type": "Point", "coordinates": [ 24.740797986112998, 41.138635978836454 ] } },
+{ "type": "Feature", "properties": { "ΟΙΚΙΣ": "Τοξότες", "ΤΟΠΙΚ": null }, "geometry": { "type": "Point", "coordinates": [ 24.790774621699505, 41.088421990272259 ] } },
+{ "type": "Feature", "properties": { "ΟΙΚΙΣ": "Κομνηνά", "ΤΟΠΙΚ": null }, "geometry": { "type": "Point", "coordinates": [ 24.727411473465231, 41.17142461790128 ] } },
+{ "type": "Feature", "properties": { "ΟΙΚΙΣ": "Άνω Καριόφυτο", "ΤΟΠΙΚ": null }, "geometry": { "type": "Point", "coordinates": [ 24.663181620715733, 41.260730449612403 ] } }
+]
+}
